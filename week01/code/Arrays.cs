4e1cbd45a9818bf0372b4acae9ff524df99e39bb
Alex <alexcondorimolle2025@gmail.com>
2026-03-13 18:36:14 -0400

    first commmit

--- week01/code/Arrays.cs
+++ week01/code/Arrays.cs
@@ -13,7 +13,14 @@ public static class Arrays
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
 
-        return []; // replace this return statement with your own
+         double[] result = new double[length];
+
+        for (int i = 1; i <= length; i++)
+        {
+            result[i - 1] = number * i;
+        }
+
+        return result; 
     }
 
     /// <summary>
@@ -29,5 +36,13 @@ public static class Arrays
         // Remember: Using comments in your program, write down your process for solving this problem
         // step by step before you write the code. The plan should be clear enough that it could
         // be implemented by another person.
+         int splitPoint = data.Count - amount;
+
+        List<int> tail  = data.GetRange(splitPoint, amount);
+        List<int> front = data.GetRange(0, splitPoint);
+
+        data.Clear();
+        data.AddRange(tail);
+        data.AddRange(front);
     }
 }
